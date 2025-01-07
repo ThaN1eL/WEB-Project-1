efadcb5af170961f9e260e02fe29fde8b97fc6c7
ThaN1eL <yuiharabusiness@gmail.com>
2025-01-07 19:15:04 +0800

    mocktest 7th +fund tranfer and bank number

--- project1/banking_program_mocktest.py
+++ project1/banking_program_mocktest.py
@@ -1,78 +1,81 @@
-                #"""MockTest from scratch: Banking Program Test 6th ATTEMPT. :)"""
+                #"""MockTest from scratch: Banking Program Test 7th ATTEMPT. :)"""
 # balance, deposit , withdraw ,fund_tranfer, bank_number and ErrorHandling
 #using define function
 
-def balance_check(balance):
-    print(f"Your current balance is ${balance}")
+def check_balance(balance):
+    print(f"Your available balance is ${balance}")
     
 def deposit(balance):
     try:
-        amount = int(input("please input the amount you want to deposit: "))
+        amount = int(input("please input how many do you want to deposit: "))
         
         if amount <= 0:
-            print("Please input amount greater than 0")
+            print("please input the value greater than 0")
             return 0
         else:
-            print(f"Congratulation you have succeed saving your money ${amount}")
+            print(f"you has been succeed deposit ${amount}")
             return amount
     except ValueError:
-        print("Please enter the numeric value")
+        print("please enter numeric value")
         return 0
     
 def withdraw(balance):
     try:
-        amount = int(input("please input the amountyou want to withdraw: "))
+        amount = int(input("please input the amount that you want to withdraw: "))
         
         if amount > balance:
             print("innuficient balance")
             return 0
         elif amount <= 0:
-            print("please input the amount greater than 0")
+            print("please input the amount greather than 0 ")
             return 0
         else:
-            print(f"Congratulation you have been succed withdrawal your money ${amount}")
+            print(f"you has been succeed withdraw ${amount}")
             return amount
     except ValueError:
-        print("Please enter numeric value")
+        print("please enter numeric value")
         return 0
+        
 def fund_tranfer(balance):
     try:
         amount = int(input("please input the amount you want to tranfer: "))
         
-        if amount > balance:
+        if amount >  balance:
             print("innuficient fund")
             return 0
         elif amount <= 0:
-            print("please enter the amounter greater than 0")
+            print("please input the value greather than 0")
             return 0
         else:
             while True:
-                bank_number = input("please input the 5-digit bank number ")
-                if bank_number.isdigit() and len(bank_number) == 5:
-                    print(f"Congratulation you have been succeed execute fund transfer ${amount} to the {bank_number}")
+                bank_number = input("please enter the 7-digit  bank number destination: ")
+                
+                if bank_number.isdigit() and len(bank_number) == 7:
+                    print(f"you has been succeed tranfer ${amount} to this bank number {bank_number}")
                     return amount
                 else:
-                    print("please enter 5 Digit bank number bro, you cant read ?!? huh???")
+                    print("please enter the valid bank number")
                     return 0
     except ValueError:
-        print("please input the numeric value")
+        print("please enter numeric value")
         return 0
-
+            
+        
 def main():
     balance = 0
     run = True
     
-    while run:  
-        print("1. Show Balance")
+    while run:
+        print("1. Balance Inquiry")
         print("2. Deposit")
         print("3. Withdraw")
         print("4. Fund Tranfer")
         print("5. Exit")
         
-        choice = input("please input, what transaction do you want to execute: ")
-        
+        choice = input("please select the number transaction do you want to execute from 1, 2, 3, 4 or 5: ")
+
         if choice == '1':
-            balance_check(balance)
+            check_balance(balance)
         elif choice == '2':
             balance += deposit(balance)
         elif choice == '3':
@@ -82,9 +85,8 @@ def main():
         elif choice == '5':
             run = False
         else:
-                print("Please enter the right choice from 1,2,3,4 or 5 :)")
-    print("ThankYOU for the today transaction :)")
-
-
+            print("please only select from 1,2,3,4 or 5 !")
+    print("Thank You for your today transaction! ")
+    
 if __name__ == '__main__':
     main()
