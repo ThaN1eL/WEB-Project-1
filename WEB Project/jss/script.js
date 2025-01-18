f099700f2c23e12d87ddc4b663ffe5db270884d5
ThaN1eL <yuiharabusiness@gmail.com>
2025-01-18 22:29:03 +0800

    setting up css for the shopping cart

--- WEB Project/jss/script.js
+++ WEB Project/jss/script.js
@@ -1,20 +1,36 @@
-// Toggle class active
+// Toggle class active hamburger menu
 
 const navbarNav = document.querySelector(' .navbar-nav');
 
 // ketika humburger menu di click
 document.querySelector('#hamburger-menu').
-onclick = () => {
+onclick = (e) => {
 navbarNav.classList.toggle('active');
+e.preventDefault();
 };
 
-// klik di luar sidebar untuk menghilangkan nav
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-const hamburger = document.querySelector
-('#hamburger-menu');
+document.querySelector('#search').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// klik di luar sidebar untuk menghilangkan nav dan search box
+
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector("#search");
 
 document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
-    }      
-})
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove("active");
+    }
+});
+
