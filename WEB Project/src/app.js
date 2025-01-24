1b59f4cded324a06a0c8e68615bfaabae1d199fe
ThaN1eL <yuiharabusiness@gmail.com>
2025-01-24 22:49:26 +0800

    alpine progress part 2

--- WEB Project/src/app.js
+++ WEB Project/src/app.js
@@ -1,10 +1,10 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
        items: [
-          { id: 1, name: 'Omnipotence Staff', img:'product2.png', price: 1000 },
-          { id: 2, name: 'Megumin Hat', img: 'product3.png', price: 300},
-          { id: 3, name: 'Omnipotence Grimoire', img: 'product1.png', price: 5555},
-          { id: 4, name: 'Holy Wand Court Emblem', img: 'product5.png', price: 2250}
+          { id: 1, name: 'Omnipotence Staff', img:'product2.png', price: "$"+1000 },
+          { id: 2, name: 'Megumin Hat', img: 'product3.png', price: "$"+300},
+          { id: 3, name: 'Omnipotence Grimoire', img: 'product1.png', price: "$"+5555},
+          { id: 4, name: 'Holy Wand Court Emblem', img: 'product5.png', price: "$"+2250}
         ]
         
     }));
