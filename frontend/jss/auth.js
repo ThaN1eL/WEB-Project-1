afdb44c9f695a881ff863a6ccc46ccd5766a06db
th4N1eL <yuiharabusiness@gmail.com>
2025-02-23 17:06:48 +0800

    test

--- frontend/jss/auth.js
+++ frontend/jss/auth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
     // Redirect to index.html
     document.querySelectorAll('#landing,#landing2').forEach(function(element) {
@@ -23,29 +22,3 @@ document.querySelectorAll('#users, #users2, #re-login').forEach(function(element
         window.location.href = 'login.html'; // Redirect to login page
     });
 });
-=======
-document.addEventListener('DOMContentLoaded', function() {
-    // Redirect to index.html
-    document.querySelectorAll('#landing,#landing2').forEach(function(element) {
-        element.addEventListener('click', function(e) {
-            e.preventDefault(); 
-            window.location.href = 'index.html';
-        });
-    });
-
-    document.querySelector('#re-signup').addEventListener('click', function(e) {
-        e.preventDefault();
-        window.location.href = 'sign_up.html';
-    });
-
-});
-
-
-// Redirect to login page when users click on the "user" icon , "Enroll NOW" button or "Already have an account?" Button
-document.querySelectorAll('#users, #users2, #re-login').forEach(function(element) {
-    element.addEventListener('click', function(e) {
-        e.preventDefault(); 
-        window.location.href = 'login.html'; // Redirect to login page
-    });
-});
->>>>>>> 83576e5 (test)
