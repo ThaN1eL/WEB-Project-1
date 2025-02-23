c0822cab9e864f5d528b866299fc067e81fa1dd1
th4N1eL <yuiharabusiness@gmail.com>
2025-02-23 16:40:24 +0800

    test

--- frontend/jss/script.js
+++ frontend/jss/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Toggle class active hamburger menu
 
 const navbarNav = document.querySelector(' .navbar-nav');
@@ -71,3 +72,78 @@ document.querySelector("form").addEventListener("submit", function(event) {
         alert("Passwords do not match. Please try again.");
     }
 });
+=======
+// Toggle class active hamburger menu
+
+const navbarNav = document.querySelector(' .navbar-nav');
+
+// ketika humburger menu di click
+document.querySelector('#hamburger-menu').
+onclick = (e) => {
+navbarNav.classList.toggle('active');
+e.preventDefault();
+};
+
+//Toggle class active shoping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// klik di luar sidebar untuk menghilangkan nav dan search box
+
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector("#search");
+const sc = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function(e) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove("active");
+    }
+
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
+});
+
+
+
+//Modal Box
+
+// Click outside to close modal
+window.onclick = (e) => {
+    const modal = document.querySelector('#item-detail-modal');
+    if (e.target === modal) {
+        Alpine.store('modal').close();
+    }
+};
+
+
+//Confirm Password
+document.querySelector("form").addEventListener("submit", function(event) {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm_password").value;
+
+    if (password !== confirmPassword) {
+        event.preventDefault(); // Prevent form submission
+        alert("Passwords do not match. Please try again.");
+    }
+});
+>>>>>>> 83576e5 (test)
